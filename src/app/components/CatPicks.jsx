b992becc80707dcfc6d9f4b6ba7cfd31bb1a43a2
Rex <kenkaneki5100@gmail.com>
2025-08-13 01:05:43 +0530

    Update

--- src/app/components/CatPicks.jsx
+++ src/app/components/CatPicks.jsx
@@ -46,36 +46,34 @@ const EverlaneFavorites = () => {
         </p>
 
         {/* Multi-Item Image Slider */}
-      <Swiper
-  modules={[Navigation, Pagination]}
-  navigation
-  pagination={{
-    clickable: true,
-    el: ".custom-pagination",
-  }}
-  spaceBetween={20}
-  loop={true}
-  slidesPerView={4}
-  slidesPerGroup={1}
-  breakpoints={{
-    320: { slidesPerView: 1.5, spaceBetween: 12 },
-    640: { slidesPerView: 2.5, spaceBetween: 16 },
-    1024: { slidesPerView: 4, spaceBetween: 20 },
-  }}
-  // Tailwind override for arrow colors
-  className="pb-10 [&_.swiper-button-next]:!text-white [&_.swiper-button-prev]:!text-white [&_.swiper-button-next]:!text-3xl [&_.swiper-button-prev]:!text-3xl [&_.swiper-button-next:hover]:scale-110 [&_.swiper-button-prev:hover]:scale-110"
->
-  {images.map((src, index) => (
-    <SwiperSlide key={index} className="flex justify-center">
-      <img
-        src={src}
-        alt={`Slide ${index + 1}`}
-        className="h-[500px] w-[300px] object-cover rounded-lg"
-      />
-    </SwiperSlide>
-  ))}
-</Swiper>
-
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation
+          pagination={{
+            clickable: true,
+            el: ".custom-pagination", // custom pagination container
+          }}
+          spaceBetween={20}
+          loop={true}
+          slidesPerView={4}
+          slidesPerGroup={1}
+          breakpoints={{
+            320: { slidesPerView: 1.5, spaceBetween: 12 },
+            640: { slidesPerView: 2.5, spaceBetween: 16 },
+            1024: { slidesPerView: 4, spaceBetween: 20 },
+          }}
+          className="pb-10"
+        >
+          {images.map((src, index) => (
+            <SwiperSlide key={index} className="flex justify-center">
+              <img
+                src={src}
+                alt={`Slide ${index + 1}`}
+                className="h-[500px] w-[300px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
         {/* Pagination Below Slider */}
         <div className="custom-pagination mt-6 flex justify-center gap-2"></div>
